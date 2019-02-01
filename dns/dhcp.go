b6df69358aefe5a2a5a14680a0c2a6285554aef2
wweir <wweir@foxmail.com>
2019-02-02 06:21:51 +0800

    Fix #2, add dhcp dynamic setting upstream dns, ipv4 only

---
+++ dns/dhcp.go
@@ -0,0 +1,85 @@
+package dns
+
+import (
+	"math/rand"
+	"net"
+
+	"github.com/golang/glog"
+	"github.com/wweir/netboot/dhcp4"
+)
+
+func GetDefaultDNSServer() string {
+	xid := make([]byte, 4)
+	rand.Read(xid)
+
+	pack := &dhcp4.Packet{
+		Type:          dhcp4.MsgDiscover,
+		TransactionID: xid,
+		Broadcast:     true,
+	}
+	options := map[dhcp4.Option][]byte{
+		dhcp4.OptRequestedOptions: []byte{byte(dhcp4.OptDNSServers)},
+	}
+
+	ifaces := mustGetInterfaces()
+	for _, iface := range ifaces {
+		conn, err := dhcp4.NewConn(iface.IP.String() + ":68")
+		if err != nil {
+			glog.Errorln(err)
+			continue
+		}
+		defer conn.Close()
+
+		pack.HardwareAddr = iface.Interface.HardwareAddr
+		options[dhcp4.OptClientIdentifier] = iface.Interface.HardwareAddr
+		pack.Options = dhcp4.Options(options)
+
+		if err := conn.SendDHCP(pack, iface.Interface); err != nil {
+			glog.Errorln(err)
+			continue
+		}
+
+		pack, _, err = conn.RecvDHCP()
+		if err != nil {
+			glog.Errorln(err)
+			continue
+		}
+
+		ip, err := pack.Options.IP(dhcp4.OptDNSServers)
+		if err != nil {
+			glog.Errorln(err)
+			continue
+		}
+		return ip.String()
+	}
+	return ""
+}
+
+type netIface struct {
+	*net.Interface
+	net.Addr
+	net.IP
+}
+
+func mustGetInterfaces() []*netIface {
+	ifaces, err := net.Interfaces()
+	if err != nil {
+		glog.Fatalln(err)
+	}
+
+	v4Iface := make([]*netIface, 0, len(ifaces))
+	for i := range ifaces {
+		if len(ifaces[i].HardwareAddr) == 0 {
+			continue
+		}
+
+		addrs, _ := ifaces[i].Addrs()
+		for _, addr := range addrs {
+			if ip := addr.(*net.IPNet).IP.To4(); ip != nil {
+				v4Iface = append(v4Iface, &netIface{&ifaces[i], addr, ip})
+				break
+			}
+		}
+	}
+	return v4Iface
+}
