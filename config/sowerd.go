af61d38b9e6bbd54d2d0a67b7ef220945a8a8f95
wweir <wweir@foxmail.com>
2025-10-27 11:28:42 +0800

    refactor: extract configuration to dedicated config package

--- config/sowerd.go
+++ config/sowerd.go
@@ -7,7 +7,7 @@ import (
 // SowerdConfig represents the configuration for sowerd daemon
 type SowerdConfig struct {
 	LogLevel slog.Level `default:"info" usage:"log level: debug, info, warn, error"`
-	ServeIP  string     `required:"true" usage:"listen to port 80 443 of the IP"`
+	ServeIP  string     `usage:"listen to port 80 443 of the IP"`
 	Password string     `required:"true"`
 	FakeSite string     `default:"/var/www" usage:"fake site address or directoy. serving on 127.0.0.1:80 if directory"`
 
